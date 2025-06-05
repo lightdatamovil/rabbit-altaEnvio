cf9b2906231c653e531c1aa835b7c984db6b9d5a
Agustt123 <agustintracheskyoficial@gmail.com>
2025-06-05 14:22:25 -0300

    Update altaEnvios-rabbit.js

--- altaEnvios-rabbit.js
+++ altaEnvios-rabbit.js
@@ -29,7 +29,7 @@ async function startConsumer() {
             return channel.ack(msg);
           }
 
-          if (idEmpresa === 159 || idEmpresa === 214 || idEmpresa === 274) {
+          if (idEmpresa === 159 || idEmpresa === 214 || idEmpresa === 274 || idEmpresa === 108) {
             const connectionDb = await getConnection(idEmpresa);
             const company = await getCompanyById(idEmpresa);
             console.log(data);
